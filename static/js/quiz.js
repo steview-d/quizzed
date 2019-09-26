1f3b7c5a543405a6d121d44b8f7118db8d206245
Dave Laffan <dave.laffan@outlook.com>
2019-09-26 15:22:28 +0100

    Store & recall player name through local storage

--- static/js/quiz.js
+++ static/js/quiz.js
@@ -1,9 +1,11 @@
 // variables
-
 var highScores = [];
+var playerName;
+
+const nameInput = document.querySelector("#playerName");
+const playerNameDiv = document.querySelector("#player-name-container");
 
 // prepare high score information
-
 if (localStorage.getItem("highScores") === null) {
     highScores = [
         { player: "arthur", score: 600 },
@@ -17,9 +19,37 @@ if (localStorage.getItem("highScores") === null) {
     highScores = JSON.parse(localStorage.getItem("highScores"));
 }
 
+// populate high score table with high score data
+if (document.getElementById("high-score-table")) {
+    highScores.map((item, idx) => {
+        document.querySelector(`#hs-n${idx + 1}`).innerHTML =
+            item.player[0].toUpperCase() + item.player.slice(1);
+        document.querySelector(`#hs-s${idx + 1}`).innerHTML = item.score;
+    });
+}
 
-highScores.map((item, idx) => {
-    document.querySelector(`#hs-n${idx + 1}`).innerHTML =
-        item.player[0].toUpperCase() + item.player.slice(1);
-    document.querySelector(`#hs-s${idx + 1}`).innerHTML = item.score;
+// remove the player name from localStorage
+function removeName() {
+    localStorage.removeItem("playerName");
+    nameInput.value = "";
+    console.log("HELLO");
+    playerNameDiv.classList.remove("hidden");
+}
+
+nameInput.value = localStorage.getItem("playerName");
+
+// check if player name in local storage
+if (localStorage.getItem("playerName") === null) {
+    playerNameDiv.classList.remove("hidden");
+} else {
+    playerNameDiv.classList.add("hidden");
+}
+
+// get player name from input form
+nameInput.addEventListener("keyup", () => {
+    if (nameInput.value === "") {
+        localStorage.removeItem("playerName");
+    } else {
+        localStorage.setItem("playerName", nameInput.value);
+    }
 });
