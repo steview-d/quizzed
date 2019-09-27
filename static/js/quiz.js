aa04fbafe5fb4b787cee6d3cb56c52ac872e88c1
Dave Laffan <dave.laffan@outlook.com>
2019-09-27 15:21:46 +0100

    Add basic layout for quiz page
    Position stat boxes across top.
    Add question section and 4 answer boxes.

--- static/js/quiz.js
+++ static/js/quiz.js
@@ -1,3 +1,14 @@
 // variables
 var highScores = JSON.parse(localStorage.getItem("highScores"));
 var playerName = localStorage.getItem("playerName");
+var score = 0;
+var multiplier = 1.0;
+
+const playerNameBox = document.querySelector('#playerNameBox > h3');
+const multiplierBox = document.querySelector('#multiplierBox > h3');
+const scoreBox = document.querySelector('#scoreBox > h3');
+
+// populate info boxes on page load
+playerNameBox.innerHTML = playerName;
+multiplierBox.innerHTML = multiplier;
+scoreBox.innerHTML = score;
