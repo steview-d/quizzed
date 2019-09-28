ee91b5d0ee1dddfefd20cf6992a445d8bad65976
Dave Laffan <dave.laffan@outlook.com>
2019-09-28 15:36:38 +0100

    Add basic Q&A routine
    App displays question, along with 4 possible answers.
    User can choose an answer, score incs if correct and moves on to the
    next question.

--- static/js/quiz.js
+++ static/js/quiz.js
@@ -3,14 +3,20 @@ var highScores = JSON.parse(localStorage.getItem("highScores"));
 var playerName = localStorage.getItem("playerName");
 var score = 0;
 var multiplier = 1.0;
-var qCount = 0; // tbc if this is even needed?
+var qNum = 0; // tbc if this is even needed?
+var qLimit = 3;
 var gameActive = true;
+var correctAnswer;
+var wrongAnswers;
+var allAnswers = [];
+var playerAnswer;
 
 var qData;
 
 // API url - this will need working on to eventually pull the data required
 // for now, 3 random questions is fine for testing
-const url = "https://opentdb.com/api.php?amount=3";
+const url =
+    "https://opentdb.com/api.php?amount=3&difficulty=easy&type=multiple";
 
 // stats constants
 const playerNameBox = document.querySelector("#playerNameBox > h3");
@@ -18,12 +24,15 @@ const multiplierBox = document.querySelector("#multiplierBox > h3");
 const scoreBox = document.querySelector("#scoreBox > h3");
 
 // q&a constants
-const qNum = document.querySelector("#q-num");
-const qText = document.querySelector("#q-text");
+const qNumBox = document.querySelector("#q-num");
+const qTextArea = document.querySelector("#q-text");
 const answer1 = document.querySelector("#answer-1");
 const answer2 = document.querySelector("#answer-2");
 const answer3 = document.querySelector("#answer-3");
 const answer4 = document.querySelector("#answer-4");
+const answerList = [answer1, answer2, answer3, answer4];
+
+const answerBoxes = document.querySelectorAll(".answer-grid-inner");
 
 // variables for testing
 const dataDisplay = document.querySelector("#data-display");
@@ -49,22 +58,89 @@ function getData(url, cb) {
     };
 }
 
+// shuffle function - Fisher - Yates
+function shuffle(a) {
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+}
+
 // ------------------------------------------------------------- Main Game Loop
 
 function gameLoop(qData) {
-    qData.forEach(question => {
-        console.log(question);
+    // qData.forEach(question => {
+    //     console.log(question);
+    // });
+
+    // ask the question
+    qNumBox.innerHTML = `Q${qNum + 1}`;
+    qTextArea.innerHTML = qData[qNum].question;
+
+    // store the correct answer
+    correctAnswer = qData[qNum].correct_answer;
+
+    // add all answers - right & wrong - to allAnswers array, and shuffle it
+    allAnswers.push(correctAnswer);
+    qData[qNum].incorrect_answers.forEach(answer => {
+        allAnswers.push(answer);
+    });
+    allAnswers = shuffle(allAnswers);
+
+    // display answers
+    allAnswers.forEach((answer, idx) => {
+        answerList[idx].innerHTML = answer;
     });
 
-    var x = 1;
+    // listen for user input when clicking an answer
+    answerBoxes.forEach(answerBox => {
+        answerBox.addEventListener("click", checkAnswer);
+    });
+}
 
-    while (gameActive) {
-        console.log(gameActive);
-        x++;
-        if (x === 5) {
-            break;
-        }
+var checkAnswer = function(e) {
+    console.log(qNum);
+
+    // remove event listeners
+    answerBoxes.forEach(answerBox => {
+        answerBox.removeEventListener("click", checkAnswer);
+    });
+
+    // check answer
+    playerAnswer = e.path[0].innerText;
+    if (playerAnswer == correctAnswer) {
+        score += 10;
     }
+
+    updateStats();
+
+    // check whether to end game or continue to next question
+    if (qNum === qData.length - 1) {
+        endGame();
+    } else {
+        qNum += 1;
+        allAnswers = [];
+
+        gameLoop(qData);
+    }
+};
+
+function endGame () {
+    console.log("THE END!!!!");
+    // next steps?
+    // hide Q&A section - keep stat boxes
+    // message saying end of game
+    // big score display
+    // options....
+    // advise if player made it onto high score table
+    // 2 options - play again (refresh page)
+    // or view high scores (load index)
+
+    // ALSO
+    // after each question attempt, show if right or wrong
+    // and have a 'next question' button
+    // either under answers, or floats at bottom of window
 }
 
 // ------------------------------------------------------ End Of Main Game Loop
